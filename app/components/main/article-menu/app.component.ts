2b227f94838420c52876bc3f1936555466e30a1f
JeanNguon <jeannguon@gmail.com>
2016-07-06 11:15:08 +0200

    Add files via upload

--- app/components/main/article-menu/app.component.ts
+++ app/components/main/article-menu/app.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //metadata object that invokes Component
 import { Component } from '@angular/core';
 
@@ -20,3 +21,27 @@ export class AppArticleMenu {
 //properties
 //application logic
  }
+=======
+//metadata object that invokes Component
+import { Component } from '@angular/core';
+
+
+
+////////////////////////<@Component>///////////////////////////////////////////
+/////decorator function that takes a metadata object as argument
+@Component({
+	//selector is a css selector for an element that represents the component
+	selector: 'article-menu',
+  	//
+  	template: '<p>article menu</p>'
+})
+
+////////////////////////<End @Component>///////////////////////////////////////////
+
+
+export class AppArticleMenu {
+
+//properties
+//application logic
+ }
+>>>>>>> 7896028cf72e47f56bea8709ebc6b68417b0651a
